3c2bf57445ef455824981a2269f5a63eac6c1f21
LucasFunti <lucasfunti@gmail.com>
2016-09-13 13:38:43 -0300

    cambio la estructura del PROC_POKEDEX, por dos procesos distintos PROC_POKEDEX_SERVIDOR y PROC_POKEDEX_SERVIDOR asi ya armo los sockets entre estos

---
+++ PROC_POKEDEX_CLIENTE/src/PROC_POKEDEX_CLIENTE.c
@@ -0,0 +1,8 @@
+/*
+ * PROC_POKEDEX_CLIENTE.c
+ *
+ *  Created on: 13/9/2016
+ *      Author: utnso
+ */
+
+
---
+++ PROC_POKEDEX_SERVIDOR/src/PROC_POKEDEX_SERVIDOR.c
@@ -0,0 +1,8 @@
+/*
+ * PROC_POKEDEX_SERVIDOR.c
+ *
+ *  Created on: 13/9/2016
+ *      Author: utnso
+ */
+
+
