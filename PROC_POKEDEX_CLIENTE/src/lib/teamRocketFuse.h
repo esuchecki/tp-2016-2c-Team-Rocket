8808ef8c6281232aa2aadea86d23f6035608d784
esuchecki <emiliano.gsuchecki@gmail.com>
2016-09-29 00:09:05 -0300

    + ejemplo_FUSE funcionando. (Ver_Maxi! -> Hay instrucciones tmb)

---
+++ PROC_POKEDEX_CLIENTE/src/lib/teamRocketFuse.h
@@ -0,0 +1,32 @@
+/*
+ * fuse.h
+ *
+ *  Created on: 28/9/2016
+ *      Author: utnso
+ */
+
+#ifndef LIB_TEAMROCKETFUSE_H_
+#define LIB_TEAMROCKETFUSE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fuse.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+
+int* pmap_pikachu;
+int* pmap_squirtle;
+int* pmap_bulbasaur;
+struct stat pikachuStat;
+struct stat squirtleStat;
+struct stat bulbasaurStat;
+
+
+int iniciarFuse(int argc,char*argv[]);
+
+
+#endif /* LIB_TEAMROCKETFUSE_H_ */
