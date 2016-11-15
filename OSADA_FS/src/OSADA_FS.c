ff5aaa2cf7acdb7f4da5e886a5cea989dd07bb7a
esuchecki <emiliano.gsuchecki@gmail.com>
2016-11-14 22:58:28 -0300

    + write | truncate a 0 y a X size -> Integrado OK (chequeado con md5sum) Nota: Probe escribir archivos pequeños, copie archivos desde mi FS al FS de OSADA, compare los md5sum, copie el special.mp4 a diferentes directorios y chequee el md5sum, chequee el truncate a 0 y el truncate a X size y parecian responder bien.. Chequee que el read siga funcioando ok y me volvio a dar todo el md5sum del challenge.bin ok!

--- OSADA_FS/src/OSADA_FS.c
+++ OSADA_FS/src/OSADA_FS.c
@@ -136,41 +136,55 @@ int main(void) {
 //	cantidadDeBloques = calcularCantidadBloques(tamanio);
 //	obtenerBloquesArchivo((int)archivos[indice].first_block, cantidadDeBloques);
 //	printf("%d\n", bloquesArchivo);
-	int indice = buscarArchivoPorPath("/Pokemons/008.txt", false);
-	int resultado = truncar("/Pokemons/008.txt", 0);
+
+//int	resultado = truncar("/prueba4.txt", 0);
+
+	int indice = buscarArchivoPorPath("/special.mp4", false);
+	//int resultado = truncar("/Pokemons/005.txt", 0);
 	osada_file* archivos = obtenerTablaArchivos();
 	printf("Tamanio Archivo: %d\n", archivos[indice].file_size);
 	printf("Primer Bloque: %d\n", archivos[indice].first_block);
-	resultado = truncar("/Pokemons/008.txt", 1500);
+	//resultado = truncar("/Pokemons/005.txt", 1600);
 	printf("Tamanio Archivo: %d\n", archivos[indice].file_size);
 	printf("Primer Bloque: %d\n", archivos[indice].first_block);
 	int cantidad = calcularCantidadBloques(archivos[indice].file_size);
+	int* tablaAsignaciones = obtenerTablaAsignaciones();
+	int revisar =1;
+	int i = archivos[indice].first_block;
+	while (tablaAsignaciones[i]!= -1)
+	{
+		i = tablaAsignaciones[i];
+		revisar++;
+	}
 	int* bloques = obtenerBloquesArchivo(archivos[indice].first_block, cantidad);
-	int i = 0;
+	i = 0;
 	//Imprimo todos los bloques que componen el archivo
 	while(i < cantidad){
 		printf("%d\n", bloques[i]);
 		i++;
 	}
 	// Escribo desde el byte 63, 20 bytes
-	resultado = escribir("/Pokemons/008.txt", "mira Emi, funciono!!", 20,63);
-	osada_block* archivo =  obtenerArchivo(bloques, cantidad, archivos[indice].file_size);
+	char * textoEjemplo = "Hay algo peor que la ejecución de proyectos inadecuados: la ausencia de ellos. En su afán de diferenciarla del miedo, el filósofo alemán Kierkegaard, definía a la angustia, inicialmente, como un temor sin objeto, como un visceral miedo a la nada. De ahí la naturaleza angustiante del hombre. En realidad me pareció oportuno retomar esta ya clásica noción de angustia, porque considero que el panorama científico a nivel nacional es, esencialmente, angustiante. Y lo es porque afrontamos la alborada de un nuevo milenio sin un proyecto rector -serio e idóneo- en materia de ciencia y técnica a nivel nacional. Aunque resulta paradójico o evidencie un acto de necedad supina, lo cierto del caso es que en medio de una crisis en la que se declaman o ponderan proyectos de crecimiento y desarrollo, en nuestra Argentina de fin de siglo la ciencia se cobija a la intemperie. No goza de estímulo real en orden a su promoción y consolidación. No figura en la agenda de prioridades, como de hecho ocurre en los paises que cotidianamente se consignan como modelos. En este contexto crucial, signado por la impronta de espacios vacíos, la universidad se referencia como un foro en donde la conquista del conocimiento es consecuencia de su convicción institucional. Así entonces, el desarrollo de la actividad científica en el sistema universitario debería ser visto como un enorme esfuerzo en respuesta a su rol fundacional, y a la vez, como una estrategia de proyección y servicio que potencia su validez y utilidad sin incentivos, sola, sin respaldo ni eje convocante.";
+	//int resultado = escribir("/README.txt", textoEjemplo, 540,0);
+	uint32_t aux;
+	osada_block* archivo = obtenerArchivoPorPath("/special.mp4",131072,131072, &aux );
+	//osada_block* archivo =  obtenerArchivo(bloques, cantidad, archivos[indice].file_size);
 	int j = 0;
 	char* array = (char*)archivo[0];
-	while(j < 20){
-		printf("%c", array[63 + j]);
+	while(j < 540){
+		printf("%c", array[0 + j]);
 		j++;
 	}
 	printf("\n");
 	// Escribo desde el byte 1000, 35 bytes
-	resultado = escribir("/Pokemons/008.txt", "otra prueba para verificar que anda", 35,1000);
-	archivo =  obtenerArchivo(bloques, cantidad, archivos[indice].file_size);
-	j = 0;
-	char* otroarray = (char*)archivo[0];
-	// Leo del byte 1000 en adelante 35 bytes
-	while(j < 35){
-		printf("%c", otroarray[1000 + j]);
-		j++;
-	}
+//	resultado = escribir("/Pokemons/008.txt", "otra prueba para verificar que anda", 35,1000);
+//	osada_block* archivo =  obtenerArchivo(bloques, cantidad, archivos[indice].file_size);
+//	j = 0;
+//	char* otroarray = (char*)archivo[0];
+//	// Leo del byte 1000 en adelante 35 bytes
+//	while(j < 35){
+//		printf("%c", otroarray[1000 + j]);
+//		j++;
+//	}
 	return EXIT_SUCCESS;
 }
