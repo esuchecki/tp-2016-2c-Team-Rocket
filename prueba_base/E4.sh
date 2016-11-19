285645e9e6395c33f5d6d6fdd191e2e056cfe727
LucasFunti <lucasfunti@gmail.com>
2016-11-19 07:40:09 -0300

    prueba_base y prueba_completa

---
+++ prueba_base/E4.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#revisar que esto tenga permisos de ejecucion..
+
+# declare MAPA_NAME variable
+ENTRENADOR_NAME="Entrenador4"
+
+# declare POKEDEX variable
+POKEDEX="/mnt/pokedex/prueba_base"
+
+# declare EXEC variable
+EXEC="~/home/utnso/tp-2016-2c-Team-Rocket/Build/"
+
+
+#limpio pantalla
+reset
+
+#print variable on a screen
+echo "abro el terminal del entrenador"
+echo "nombreEntrenador:  "$ENTRENADOR_NAME
+echo "ubicacion_pokedex:  "$POKEDEX
+echo "***********************************"
+eval $EXEC" "$ENTRENADOR_NAME" "$POKEDEX
---
+++ prueba_completa/mAzul.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#***************
+#instrucciones:
+#revisar que esto tenga permisos de ejecucion..
+#copiar en /home/utnso/
+#abrir el terminal y escribir "./iniciarMapa.sh"
+#listo....
+#***************
+
+
+
+# declare MAPA_NAME variable
+MAPA_NAME="Azul"
+
+# declare POKEDEX variable
+POKEDEX="/mnt/pokedex/prueba_completa"
+
+# declare EXEC variable
+EXEC="~/home/utnso/tp-2016-2c-Team-Rocket/Build/"
+
+
+#limpio pantalla
+reset
+
+#print variable on a screen
+echo "abro el terminal del mapa"
+echo "nombreMapa:  "$MAPA_NAME
+echo "ubicacion_pokedex:  "$POKEDEX
+echo "***********************************"
+eval $EXEC" "$MAPA_NAME" "$POKEDEX
